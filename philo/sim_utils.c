fbc0ac4a1b4e78156f0a97317f6f5e38565c1041
steffli <stephan-liu@hotmail.de>
2025-12-03 13:15:46 +0100

    added simulation utils to check if philo died therefore locking mutex to avoid data races, also start simulation to create threads for every philo at the table. philo routine still needs to be implemented

---
+++ philo/sim_utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sim_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stliu <stliu@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 23:18:57 by stliu             #+#    #+#             */
+/*   Updated: 2025/12/02 23:18:57 by stliu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static	bool died(t_philo *philo)
+{
+	long	time;
+
+	pthread_mutex_lock(&philo->table->death_lock);
+	time = get_time() - philo->last_meal;
+	if (time > philo->table->time_to_die)
+	{
+		philo->table->dead = 1;
+		pthread_mutex_unlock(&philo->table->death_lock);
+		pthead_mutex_lock(&philo->table->write_lock);
+		printf("%ld %d died\n", get_time() - philo->table->start_time, philo->id);
+		pthread_mutex_unlock(&philo->table->write_lock);
+		return (true);
+	}
+	pthread_mutex_unlock(&philo->table->death_lock);
+	return (false);
+}
+static void	monitor(t_table *table)
+{
+	int i;
+
+	while(1)
+	{
+		i = 0;
+		while (table->n_philos)
+		{
+			if (died(&table->philos[i]))
+				return ;
+			i++;
+		}
+		if (check_all_ate(table))
+		{
+			pthread_mutex_lock(&table->death_lock);
+			table->dead = 1;
+			pthread_mutex_unlock(&table->death_lock);
+			return ;
+		}
+		usleep(100);
+	}
+}
+void	start_sim(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	table->start_time = get_time();
+	while (i < table->n_philos)
+	{
+		table->philos[i].last_meal = get_time();
+		pthread_create(&table->philos[i].thread, NULL,
+			philo_routine, &table->philos[i]);
+		i++;
+	}
+	monitor(table);
+	i = 0;
+	while (table->n_philos)
+	{
+		pthread_join(table->philos[i].thead, NULL);
+		i++;
+	}
+}
